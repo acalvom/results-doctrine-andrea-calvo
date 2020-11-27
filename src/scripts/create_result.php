daa4bc4bbc36c22d17e9ceb541b6d09603d4df7d
acalvom <andreac.3108@gmail.com>
2020-11-27 11:39:49 +0100

    create_result.php

--- src/scripts/create_result.php
+++ src/scripts/create_result.php
@@ -24,7 +24,7 @@ if ($argc < 3 || $argc > 4) {
     $fich = basename(__FILE__);
     echo <<< MARCA_FIN
 
-    Usage: $fich <Result> <UserId> [<Timestamp>]
+    Usage: $fich <Result> <UserId> [<Timestamp>] [--json]
 
 MARCA_FIN;
     exit(0);
@@ -32,7 +32,24 @@ MARCA_FIN;
 
 $newResult    = (int) $argv[1];
 $userId       = (int) $argv[2];
-$newTimestamp = $argv[3] ?? new DateTime('now');
+
+switch ($argc){
+    case 3:
+        $newTimestamp = new DateTime('now');
+        break;
+
+    case 4:
+        if($argv[3] == '--json'){
+            $newTimestamp = new DateTime('now');
+        } else{
+            $newTimestamp = $argv[3];
+        }
+        break;
+
+    case 5:
+        $newTimestamp = $argv[3];
+        break;
+}
 
 /** @var User $user */
 $user = $entityManager
@@ -47,8 +64,12 @@ $result = new Result($newResult, $user, $newTimestamp);
 try {
     $entityManager->persist($result);
     $entityManager->flush();
-    echo 'Created Result with ID ' . $result->getId()
-        . ' USER ' . $user->getUsername() . PHP_EOL;
+    if (in_array('--json', $argv)) {
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    } else {
+        echo 'Created Result with Id ' . $result->getId()
+            . ' USER ' . $user->getUsername() . PHP_EOL;
+    }
 } catch (Throwable $exception) {
     echo $exception->getMessage();
 }
