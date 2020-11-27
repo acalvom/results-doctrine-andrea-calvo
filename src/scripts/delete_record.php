18cb89f1ddfeb108b8bce65b02953410b9e34c1e
acalvom <andreac.3108@gmail.com>
2020-11-27 12:35:15 +0100

    create delete_record.php to remove a single record by id

---
+++ src/scripts/delete_record.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * PHP version 7.4
+ * src/create_user_admin.php
+ *
+ * @category Utils
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://www.etsisi.upm.es/ ETS de Ingeniería de Sistemas Informáticos
+ */
+
+use MiW\Results\Entity\Result;
+use MiW\Results\Utility\Utils;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+Utils::loadEnv(__DIR__ . '/../../');
+
+$entityManager = Utils::getEntityManager();
+
+if ($argc !=2 ) {
+    $fich = basename(__FILE__);
+    echo <<< MARCA_FIN
+
+    Usage: $fich <ResultId>
+
+MARCA_FIN;
+    exit(0);
+}
+
+$resultId = (int) $argv[1];
+
+/** @var Result $result */
+$result = $entityManager
+    ->getRepository(Result::class)
+    ->findOneBy(['id' => $resultId]);
+if (null === $result) {
+    echo "Result ($resultId) not found" . PHP_EOL;
+    exit(0);
+}
+
+$entityManager->remove($result);
+$entityManager->flush();
+
+$deletedResult = $entityManager
+    ->getRepository(Result::class)
+    ->findOneBy(['id' => $resultId]);
+if (null === $deletedResult) {
+    echo "Result has been deleted successfully" . PHP_EOL;
+}
